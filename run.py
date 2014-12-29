9ea4cec047d6169dbd76a636161267a98d4d1900
BethJohnson <BethJohnson3@gmail.com>
2014-12-29 15:55:10 -0800

    Fixes 405 bug, modifies parsing of post data. closes #26

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ app = Flask(__name__, static_folder='client', static_url_path='')
 def root():
 	return app.send_static_file('index.html')
 
-@app.route('/drivers', methods=['GET', 'POST'])
+@app.route('/driver', methods=['GET', 'POST'])
 def drivers():
 	if (request.method == 'GET'):
 		origin = request.args.get('origin')
@@ -25,7 +25,7 @@ def drivers():
 			connect.createDriver(data['origin'], data['destination'], data['id'])
 			return 'Driver added to database'
 
-@app.route('/passengers', methods=['GET', 'POST'])
+@app.route('/passenger', methods=['GET', 'POST'])
 def passengers():
 	if (request.method == 'GET'):
 		origin = request.args.get('origin')
@@ -33,9 +33,9 @@ def passengers():
 		temp = connect.findPassengers(origin, destination)
 		return str(temp)
 	if (request.method == 'POST'):
-		if (request.headers['Content-Type'] == 'application/json'):
+		if (request.headers['Content-Type'] == 'application/json;charset=UTF-8'):
 			data = json.loads(request.data)
-			connect.createPassenger(data['origin'], data['destination'], data['id'])
+			connect.createPassenger([data['origin']['lat'],data['origin']['lon']], [data['destination']['lat'],data['destination']['lon']], 1)
 			return 'Passenger added to database'
 
 if (__name__ == '__main__'):
